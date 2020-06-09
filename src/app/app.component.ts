ceeee762c37380d287661c2cc6726436e19ba1e8
CaesiumY <mn0658@naver.com>
2020-06-09 14:41:29 +0900

    chore: Add firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,54 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
-import { HomePage } from '../pages/home/home';
+import { HomePage } from "../pages/home/home";
+import { LoginPage } from "../pages/login/login";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBbWdFogmNWrAcbvt8SG8Lm4XsoxPcyJzU",
+  authDomain: "ionic-with-firebase-3234c.firebaseapp.com",
+  databaseURL: "https://ionic-with-firebase-3234c.firebaseio.com",
+  projectId: "ionic-with-firebase-3234c",
+  storageBucket: "ionic-with-firebase-3234c.appspot.com",
+  messagingSenderId: "782825335965",
+  appId: "1:782825335965:web:5bb33ed19335c09a5df874",
+  measurementId: "G-ML2PTKNBRE",
+};
+// Initialize Firebase
+
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html",
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen
+  ) {
     platform.ready().then(() => {
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        // User is signed in.
+        this.rootPage = HomePage;
+      } else {
+        // User is signed out.
+        this.rootPage = LoginPage;
+      }
+    });
   }
 }
-
