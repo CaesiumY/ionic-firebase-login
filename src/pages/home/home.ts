e1563d4aee07af6a41df785336debe6ca9917183
CaesiumY <mn0658@naver.com>
2020-06-19 12:54:30 +0900

    feat: Add logout in home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import * as firebase from "firebase";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html",
 })
 export class HomePage {
+  constructor(public navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
-
+  logout() {
+    firebase
+      .auth()
+      .signOut()
+      .then(function () {
+        // Sign-out successful.
+        console.log("logout successfully");
+      })
+      .catch(function (error) {
+        // An error happened.
+        console.log("HomePage -> logout -> error", error);
+      });
   }
-
 }
