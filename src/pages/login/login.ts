65616ee6d1334c5b7a2c4fef979cf19ed7df1ec6
CaesiumY <mn0658@naver.com>
2020-06-20 12:44:15 +0900

    feat: Add reset password

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,10 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  AlertController,
+} from "ionic-angular";
 import { SignupPage } from "../signup/signup";
 import * as firebase from "firebase";
 import { LoaderProvider } from "../../providers/loader/loader";
@@ -25,7 +30,8 @@ export class LoginPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private loader: LoaderProvider
+    private loader: LoaderProvider,
+    private alertCtrl: AlertController
   ) {}
 
   ionViewDidLoad() {
@@ -54,5 +60,53 @@ export class LoginPage {
   }
   signup() {
     this.navCtrl.push(SignupPage);
+  }
+
+  reset() {
+    let alert = this.alertCtrl.create({
+      title: "Reset Passworld",
+      message: "패스워드 재설정 링크를 받을 이메일 주소를 적어주세요.",
+      inputs: [
+        {
+          name: "email",
+          placeholder: "email",
+        },
+      ],
+      buttons: [
+        {
+          text: "취소",
+          role: "cancel",
+          handler: () => {
+            console.log("cancelled");
+          },
+        },
+        {
+          text: "확인",
+          handler: (data) => {
+            var auth = firebase.auth();
+            var emailAddress = data.email;
+
+            auth
+              .sendPasswordResetEmail(emailAddress)
+              .then(() => {
+                // Email sent.
+                let alert = this.alertCtrl.create({
+                  title: "Password Reset Email",
+                  subTitle:
+                    "사용자가 입력한 이메일로 패스워드 재설정 메일이 전송되었습니다. 메일을 확인해 주세요.",
+                  buttons: ["확인"],
+                });
+                alert.present();
+              })
+              .catch((error) => {
+                // An error happened.
+                console.warn(error);
+              });
+          },
+        },
+      ],
+    });
+
+    alert.present();
   }
 }
